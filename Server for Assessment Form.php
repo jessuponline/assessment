f28db1b55ffa64cd581d31d407df5cfdc0a5817a
Matthew Timms <mtimms@wju.univ>
2017-11-03 10:07:39 -0700

    Changing to get method to test for 405 error.

--- Server for Assessment Form.php
+++ Server for Assessment Form.php
@@ -1,12 +1,12 @@
 <?php
-if($_POST['formSubmit'] == "Submit")
+if($_GET['formSubmit'] == "Submit")
 {
   $errorMessage = "";
-  if(empty($_POST['answer1']))
+  if(empty($_GET['answer1']))
   {
     $errorMessage .= "<li>You forgot to respond to this question!</li>";
   }
-  $varAnswer1 = $_POST['answer1'];
+  $varAnswer1 = $_GET['answer1'];
   if(!empty($errorMessage))
   {
     echo("<p>There was an error with your form:</p>\n");
