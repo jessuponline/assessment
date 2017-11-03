599587ce560cb6f21c08189baa0e23820f1916be
Matthew Timms <mtimms@wju.univ>
2017-11-03 10:08:50 -0700

    Back to push

--- Server for Assessment Form.php
+++ Server for Assessment Form.php
@@ -1,12 +1,12 @@
 <?php
-if($_GET['formSubmit'] == "Submit")
+if($_PUSH['formSubmit'] == "Submit")
 {
   $errorMessage = "";
-  if(empty($_GET['answer1']))
+  if(empty($_PUSH['answer1']))
   {
     $errorMessage .= "<li>You forgot to respond to this question!</li>";
   }
-  $varAnswer1 = $_GET['answer1'];
+  $varAnswer1 = $_PUSH['answer1'];
   if(!empty($errorMessage))
   {
     echo("<p>There was an error with your form:</p>\n");
